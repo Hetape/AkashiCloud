46bfe1b245fa6cc2a99fd349a8bab544e5e4c043
Hetape <hetape.october@gmail.com>
2023-05-11 03:13:49 -0400

    landingpage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { Navbar } from './Component/Navbar';
-import { Landing } from './Component/Landing';
+import { Dashboard } from './Component/Dashboard';
+import {Landing} from './Component/Landing'
 import { ThemeProvider } from "./ThemeContext";
 function App() {
   
   return (
     <ThemeProvider>
       <div className="App">
-        <Navbar/>
+        {/* <Navbar/>
+        <Dashboard/> */}
         <Landing/>
       </div>
     </ThemeProvider>
--- src/Component/Landing.js
+++ src/Component/Landing.js
@@ -1,65 +1,44 @@
-import ApexChart from "./ApesCharts";
-import { useTheme } from '../ThemeContext';
 export const Landing = () => {
-    const {theme} = useTheme()
-    return (
-        <>          
-            <div className={`bg-custom-color ${theme==='dark' ? 'text-white bg-dark-custom-color' : 'text-gray-900 bg-custom-color'} flex flex-col items-center lg:py-32 lg:justify-center`}>
-                <div className="w-full px-10">
-                    <div className="bg-transparent flex justify-center py-10">
-                        <div className='flex lg:hidden bg-cover bg-center w-24 h-8 bg-[url("./img/Akashi4_1.png")]'></div>
+    return(
+        <>
+           <div className="flex flex-col justify-center items-center md:flex-row bg-custom-color w-full md:py-3 md:justify-between">
+                <div className="flex flex-col md:flex-row justify-center items-center md:justify-start">
+                    <div className='bg-auto bg-cover w-[140px] h-[90px] bg-[url("./img/Akashism.png")]'></div>
+                    <div className="flex flex-col md:py-5 items-center justify-center">
+                        <div className='bg-auto bg-cover w-[140px] h-[48px] bg-[url("./img/Akashism_1.png")]'></div>
+                        <div className='text-left font-bold'>
+                            A reward-based token ecosystem<br/>on the Ethereum blockchain.
+                        </div>
                     </div>
-                    <div className="flex flex-col lg:flex lg:flex-row items-center lg:justify-center">
-                        <div className="lg:order-1 order-2  sm:w-[520px] px-3">
-                            <ApexChart/>
-                            <div className="mb-[30px] py-4 flex flex-col bg-transparent border-2 rounded-3xl px-8"
-                            style={{boxShadow: "-4px 0px 4px rgba(0, 0, 0, 0.25), 0px -4px 4px rgba(0, 0, 0, 0.25), 4px 0px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25)"}}>
-                                <div className="flex flex-row justify-between">
-                                    <div>AKASHI Holders</div>
-                                    <div>1,263</div>
-                                </div>
-                                <div className="py-4 flex flex-row justify-around">
-                                <button className="w-32 h-10 rounded-full text-white drop-shadow-[0_3px_0px_rgba(0,0,0,1)]" style={{background:"linear-gradient(180deg, #CC6832 0%, rgba(204, 104, 50, 0.6) 100%)"}}>Buy</button>
-                                <button className="w-32 h-10 rounded-full text-white drop-shadow-[0_3px_0px_rgba(0,0,0,1)]" style={{background:"linear-gradient(180deg, #CC6832 0%, rgba(204, 104, 50, 0.6) 100%)"}}>Burn</button>
-                                <button className="w-32 h-10 rounded-full text-white drop-shadow-[0_3px_0px_rgba(0,0,0,1)]" style={{background:"linear-gradient(180deg, #CC6832 0%, rgba(204, 104, 50, 0.6) 100%)"}}>Supply</button>
-                                </div>
-                                    
+                </div>
+                <div className="flex flex-col md:flex-row md:py-5">
+                    <div className="md:mr-10 flex flex-col justify-center md:justify-between">
+                        <div className="flex flex-col items-center md:flex-row justify-center">
+                            <span>Audit By:</span>
+                            <div className='-mt-1 w-24 h-8 bg-auto bg-cover bg-[url("./img/certik.png")]'></div>
+                        </div>
+                        <div className="flex flex-col md:flex-row items-center justify-center ">
+                            <span>Listed On:</span>
+                            <div className="flex flex-row">
+                                <div className='mr-5 w-5 h-5 bg-auto bg-cover bg-[url("./img/coinmarketcap.png")]'></div>
+                                <div className='w-5 h-5 bg-auto bg-cover bg-[url("./img/coingecko.png")]'></div>
                             </div>
                         </div>
-                        <div className="flex flex-col order-1 lg:order-2  sm:w-[520px]">
-                            <div className="mb-[30px] py-4 bg-transparent border-2 rounded-3xl "
-                            style={{boxShadow: "-4px 0px 4px rgba(0, 0, 0, 0.25), 0px -4px 4px rgba(0, 0, 0, 0.25), 4px 0px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25)"}}>
-                                <div class="grid lg:grid-cols-2 lg:grid-rows-2 lg:py-4 lg:gap-x-44 gap-3 grid-cols-1 grid-rows-4 px-8">
-                                    <div className="text-center lg:text-left">Total Supply</div>
-                                    <div className="text-center lg:text-right">500,000,000,000,000</div>
-                                    <div className="text-center lg:text-left">Liquid Supply</div>
-                                    <div className="text-center lg:text-right ">15,000,000,000,000</div>
-                                </div>
-                            </div>
-
-                            <div className="mb-[30px] py-4 bg-transparent border-2 rounded-3xl "
-                            style={{boxShadow: "-4px 0px 4px rgba(0, 0, 0, 0.25), 0px -4px 4px rgba(0, 0, 0, 0.25), 4px 0px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25)"}}>
-                                    <div class="grid lg:grid-cols-2 lg:grid-rows-4 lg:py-4 gap-3 grid-cols-1 grid-rows-8 px-8">
-                                    <div className="text-center lg:text-left">Total Liquidity</div>
-                                    <div className="text-center lg:text-right">$50,000</div>
-                                    <div className="text-center lg:text-left">Treasury Balance</div>
-                                    <div className="text-center lg:text-right">5,000,000,000,000</div>
-                                    <div className="text-center lg:text-left">AKASHI Burned</div>
-                                    <div className="text-center lg:text-right">2,000,000</div>
-                                    <div className="text-center lg:text-left">Rewards Distributed</div>
-                                    <div className="text-center lg:text-right">200,000,000</div>
-                                </div>
-                            </div>
-                            <div className="py-4 bg-transparent border-2 rounded-3xl "
-                            style={{boxShadow: "-4px 0px 4px rgba(0, 0, 0, 0.25), 0px -4px 4px rgba(0, 0, 0, 0.25), 4px 0px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25)"}}>
-                                    <div class="grid lg:grid-cols-2 lg:grid-rows-2 lg:py-4 gap-3  grid-cols-1 grid-rows-4 px-8">
-                                    <div className="text-center lg:text-left">AKASHIx Minted</div>
-                                    <div className="text-center lg:text-right">0</div>
-                                    <div className="text-center lg:text-left">AKASHIx Staked</div>
-                                    <div className="text-center lg:text-right">0</div>
-                                </div>
-                            </div>
+                        <div className="md:mt-2 flex flex-row justify-center items-center mt-28 md:px-3">
+                            <div className='mr-7 w-[27px] h-5 bg-auto bg-cover bg-[url("./img/twitter.png")]'></div>
+                            <div className='mr-7 w-[21px] h-[17px] bg-auto bg-cover bg-[url("./img/telegram.png")]'></div>
+                            <div className='mr-7 w-[24px] h-[17px] bg-auto bg-cover bg-[url("./img/discord.png")]'></div>
+                            <div className='mr-7 w-5  h-5 bg-auto bg-cover bg-[url("./img/reddis.png")]'></div>
+                            <div className='mr-7 w-4 h-5 bg-auto bg-cover bg-[url("./img/em.png")]'></div>
                         </div>
+                    </div>
+                    <div className="flex flex-col md:text-left text-center">
+                        <div className="flex md:flex-row justify-center my-5 md:mt-2">
+                            <button className="mr-2 w-[130px] h-[35px] rounded-full text-white drop-shadow-[0_3px_0px_rgba(0,0,0,1)]" style={{background:"linear-gradient(180deg, #CC6832 0%, rgba(204, 104, 50, 0.6) 100%)"}}>Launch Dapp</button>
+                            <button className="w-[130px] h-[35px] rounded-full text-white drop-shadow-[0_3px_0px_rgba(0,0,0,1)]" style={{background:"linear-gradient(180deg, #CC6832 0%, rgba(204, 104, 50, 0.6) 100%)"}}>Buy Akashi</button>
+                        </div>
+                        <span>AKASHI: 0x00000000000000000000000000000</span>
+                        <span>Copyright © 2023 by Akashi Cloud. All Rights Reserved. </span>
                     </div>
                 </div>
             </div>
